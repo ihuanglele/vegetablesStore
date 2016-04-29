80fa83178e81d835f22cc43a5a66ee7fd0b19052
ihuanglele <844649263@qq.com>
2016-04-29 10:06:06 +0800

    没有什么好写的，创建项目，完了部分的设计，本次提交是为了备份。

---
+++ App/Admin/Conf/config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+
+    //开启layout布局
+    'LAYOUT_ON'=>true,
+    'LAYOUT_NAME'=>'Public/layout',
+
+);
--- App/Common/Conf/config.php
+++ App/Common/Conf/config.php
@@ -7,6 +7,13 @@ return array(
     'DB_PWD' => 'vegetables123',
 
     'SHOW_PAGE_TRACE' => true,
+    //定义模板信息
+    'TMPL_L_DELIM' => '<{',
+    'TMPL_R_DELIM' => '}>',
+
+    //定义模块
+    'MODULE_ALLOW_LIST'    =>    array('Home','Admin'),
+    'DEFAULT_MODULE'       =>    'Home',
 
     //用户状态
     'UserStatue' => array(
@@ -32,6 +39,18 @@ return array(
     'OrderType' => array(
         '1' => '自己购买',
         '2' => '送人',
+    ),
+
+    //微信支付状态
+    'wxPayStatus' => array(
+        '1' => '待支付',
+        '2' => '已支付',
+    ),
+
+    //用户财务记录类型
+    'MoneyType' => array(
+        '1' => '付款',
+        '2' => '充值',
     ),
 
 );
